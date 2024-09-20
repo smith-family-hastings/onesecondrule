1dbff266fdbfb52fced6571234ff6f686a28133f
admin <kvsmith90@proton.me>
2024-09-20 07:24:06 -0700

    fixes

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Gumroad
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths += Dir["#{config.root}/app/services"]
   end
 end
